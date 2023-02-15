2bd9125fef87db33badf5f56912e1e6b8029ad79
ptaseta <Waanru616@gmail.com>
2023-02-15 13:00:52 +0100

    Barra de búsqueda y poco más

--- src/main/java/com/wordle/recuperacion/controllers/WordleController.java
+++ src/main/java/com/wordle/recuperacion/controllers/WordleController.java
@@ -2,6 +2,7 @@ package com.wordle.recuperacion.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,10 +10,10 @@ public class WordleController {
     
     @GetMapping("/")
     public String goToIndex(){
-        return "index1";
+        return "index";
     }
 
-    @GetMapping("/index")
+    @PostMapping("/")
     public ModelAndView index(ModelAndView mv) {
         mv.setViewName("index");
         return mv;
